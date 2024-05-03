2107219ede12edbf91ccba122ed30a395cb10254
superKazi <kazi.awal@journalism.cuny.edu>
2024-05-03 14:41:42 -0400

    get fonts working, add images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import "./reset.css";
+	import './global.css';
 </script>
 
 <svelte:head>
@@ -7,8 +7,32 @@
 </svelte:head>
 
 <main>
-	<h1>
-		Hello world
-	</h1>
+	<hgroup>
+		<p class="chel-bold kicker">opinion</p>
+		<h1 class="chel-dd">
+			To<br />fall<br />in love<br />with<br />the<br />world
+		</h1>
+		<p class="chel-light dek">
+			A late summer meditation<br />on time, loss, and solace in<br />the natural world, made from<br
+			/>the work of the photographer<br />Mary Manning and the<br />poet Brian Turner.
+		</p>
+	</hgroup>
 </main>
 
+<style>
+	.kicker {
+		text-transform: uppercase;
+		letter-spacing: 0.05em;
+		line-height: 1;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		letter-spacing: 0.3em;
+		line-height: 1.3;
+	}
+
+	.dek {
+		line-height: 1.2;
+	}
+</style>
